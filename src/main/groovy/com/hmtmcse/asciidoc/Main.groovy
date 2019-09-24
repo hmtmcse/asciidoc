990a6ff48cc2992b1fcd943b38872ebfa99b1237
Touhid Mia <touhid@bitmascot.com>
2019-09-24 18:26:08 +0600

    DevOps Tool

---
+++ src/main/groovy/com/hmtmcse/asciidoc/Main.groovy
@@ -0,0 +1,26 @@
+package com.hmtmcse.asciidoc
+
+import org.asciidoctor.Asciidoctor
+
+class Main {
+
+
+    public static void main(String[] args) {
+        Asciidoctor asciidoctor = Asciidoctor.Factory.create()
+
+
+        String html = asciidoctor.convert(
+                "Writing AsciiDoc is _easy_!",
+                new HashMap<String, Object>())
+        System.out.println(html)
+
+
+         html = asciidoctor.convertFile(
+                new File("all-plugins/asciidoc/asciidoc/index.adoc"),
+                new HashMap<String, Object>());
+        System.out.println(html);
+
+    }
+
+
+}
--- src/main/java/com/hmtmcse/asciidoc/Main.java
+++
@@ -1,9 +0,0 @@
-package com.hmtmcse.asciidoc;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Bismillah.");
-    }
-
-}
