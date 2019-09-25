6eb1d7ffb156fdfdbac66c0172219a0c8b7f1b1e
Touhid Mia <hmtm.cse@gmail.com>
2019-09-25 06:24:33 +0600

    Update Resolver

---
+++ src/main/groovy/com/hmtmcse/asciidoc/AdocConverter.groovy
@@ -0,0 +1,34 @@
+package com.hmtmcse.asciidoc
+
+import com.hmtmcse.jtfutil.text.ReadWriteTextFile
+import com.hmtmcse.jtfutil.text.TextFileData
+import org.asciidoctor.Asciidoctor
+
+class AdocConverter {
+
+    public aDocOptions = new HashMap<String, Object>()
+    public ReadWriteTextFile readWriteTextFile = null
+
+    public AdocConverter(){
+        this.readWriteTextFile = new ReadWriteTextFile()
+    }
+
+    public Asciidoctor getAdoc(){
+        return Asciidoctor.Factory.create()
+    }
+
+    public String getHtmlFromText(String text){
+        return getAdoc().convert(text, aDocOptions)
+    }
+
+
+    public String getHtmlFromFile(String location){
+        try{
+            TextFileData textFileData = readWriteTextFile.readFileToString(location)
+            return getHtmlFromText(textFileData.text)
+        }catch(Exception e){
+            return ""
+        }
+    }
+
+}
