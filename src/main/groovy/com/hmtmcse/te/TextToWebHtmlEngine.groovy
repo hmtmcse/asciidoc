7c27cfa9533ba7c5bde6a0e162a072bfbbfba0e5
Touhid Mia <hmtm.cse@gmail.com>
2020-03-21 22:43:15 +0600

    Text to web engine

--- src/main/groovy/com/hmtmcse/te/TextToWebHtmlEngine.groovy
+++ src/main/groovy/com/hmtmcse/te/TextToWebHtmlEngine.groovy
@@ -6,9 +6,11 @@ import com.hmtmcse.fileutil.fd.FDUtil
 import com.hmtmcse.fileutil.fd.FileDirectory
 import com.hmtmcse.fileutil.text.TextFile
 import com.hmtmcse.te.data.InternalResponse
+import com.hmtmcse.te.data.TextToWebEngineData
 import com.hmtmcse.texttoweb.Config
 import com.hmtmcse.texttoweb.TextToWebConst
 import com.hmtmcse.texttoweb.common.ConfigLoader
+import com.hmtmcse.texttoweb.processor.GenerateProcessor
 import com.hmtmcse.tmutil.TStringUtil
 import groovy.text.SimpleTemplateEngine
 
@@ -17,6 +19,7 @@ class TextToWebHtmlEngine {
     private FileDirectory fileDirectory
     private TextFile textFile
     private Config config
+    private String slash = "/"
 
 
     public TextToWebHtmlEngine() {
@@ -31,6 +34,35 @@ class TextToWebHtmlEngine {
 
     private String concatPath(String path) {
         return FDUtil.concatPath(config.source, path)
+    }
+
+    private String urlToPath(String url) {
+        return TStringUtil.findReplace(url, slash, File.separator)
+    }
+
+    public TextToWebEngineData getDescriptorData(String url, String relativePath, String descriptorName) {
+        List<String> urlFragments = TStringUtil.splitAsList(url, slash)
+        urlFragments = TStringUtil.reverseList(urlFragments)
+
+        TextToWebEngineData textToWebEngineData = new TextToWebEngineData()
+        textToWebEngineData.url = url
+        textToWebEngineData.relativePath = relativePath
+
+        String descriptorFile, temp
+        GenerateProcessor generateProcessor = new GenerateProcessor()
+        if (urlFragments) {
+            for (String fragment : urlFragments) {
+                descriptorFile = FDUtil.concatPath(concatPath(relativePath), descriptorName)
+                if (fileDirectory.isDirectory(descriptorFile)) {
+                    textToWebEngineData.descriptor = generateProcessor.loadYmlFromFile(descriptorFile)
+                    return textToWebEngineData
+                }
+                temp = File.separator + fragment
+                relativePath = relativePath.substring(0, (relativePath.length() - temp.length()))
+            }
+        }
+        textToWebEngineData.descriptor = generateProcessor.loadYmlFromFile(concatPath(descriptorName))
+        return textToWebEngineData
     }
 
 
@@ -52,7 +84,8 @@ class TextToWebHtmlEngine {
                 throw new AsciiDocException("Empty URL")
             }
             String trimUrl = TStringUtil.trimStartEndChar(url, "/")
-            String path = concatPath(trimUrl)
+            String urlToPath = urlToPath(trimUrl)
+            String path = concatPath(urlToPath)
             InternalResponse internalResponse = getDescriptorName(path)
             println(path)
 
---
+++ src/main/groovy/com/hmtmcse/te/data/TextToWebEngineData.groovy
@@ -0,0 +1,15 @@
+package com.hmtmcse.te.data
+
+import com.hmtmcse.texttoweb.Descriptor
+
+class TextToWebEngineData {
+
+    public Descriptor descriptor
+    public String layout;
+    public String urlKey;
+    public String url;
+    public String relativePath;
+    public String absolutePath;
+    public List<String> topicNav = [];
+
+}
