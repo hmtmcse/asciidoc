85d1eade13fe4049abdea968e2caeb815847d412
hmtmcse <hmtm.cse@gmail.com>
2020-04-17 22:17:05 +0600

    header tag

--- src/main/groovy/com/hmtmcse/texttoweb/processor/SeoProcessor.groovy
+++ src/main/groovy/com/hmtmcse/texttoweb/processor/SeoProcessor.groovy
@@ -1,9 +1,40 @@
 package com.hmtmcse.texttoweb.processor
 
+import com.hmtmcse.common.AsciiDocException
+import com.hmtmcse.te.TextToWebHtmlEngine
+import com.hmtmcse.te.data.TextToWebEngineConfig
+import com.hmtmcse.te.data.TextToWebEngineData
+import com.hmtmcse.texttoweb.Seo
+import com.hmtmcse.texttoweb.Tag
 import com.hmtmcse.texttoweb.model.CommandProcessor
 
 class SeoProcessor implements CommandProcessor {
 
+
+    Seo getSeoFromTextToWebEngineData(TextToWebEngineData textToWebEngineData) {
+        if (textToWebEngineData.topicNav.meta && textToWebEngineData.topicNav.meta.get(textToWebEngineData.urlKey)?.seo) {
+            return textToWebEngineData.topicNav.meta.get(textToWebEngineData.urlKey).seo
+        }
+        return new Seo()
+    }
+
+    Seo getSeoSetting(String url) throws AsciiDocException {
+        if (!url) {
+            throw new AsciiDocException("Empty URL")
+        }
+        TextToWebHtmlEngine textToWebHtmlEngine = new TextToWebHtmlEngine()
+        TextToWebEngineConfig config = new TextToWebEngineConfig()
+        TextToWebEngineData textToWebEngineData = textToWebHtmlEngine.processTextToWebEngineData(url, config)
+        Seo seo = getSeoFromTextToWebEngineData(textToWebEngineData)
+        if (!seo.title) {
+            seo.title = textToWebHtmlEngine.getPageTitle(textToWebEngineData, config)
+        }
+
+        if (!seo.tags) {
+            seo.addTags(new Tag().description(""))
+        }
+        return seo
+    }
 
 
     String getSeoEditor(String url = ""){
